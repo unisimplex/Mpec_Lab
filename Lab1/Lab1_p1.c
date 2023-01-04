c62fa9327db0fa15825f2a1e11a4fbc83acf8298
Unisimplex <119426922+unisimplex@users.noreply.github.com>
2023-01-04 22:26:52 +0530

    Add files via upload

---
+++ Lab1/Lab1_p1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main(){
+	int p,r,t,si;
+	printf("Enter Principal , Rate of interest and Time \n");
+	scanf("%d%d%d",&p,&r,&t);
+	si = (p*r*t)/100;
+	printf("Simple Interest = %d",si);
+}
---
+++ Lab1/Lab1_p2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+	float s1,s2,s3,s4,s5,sum ;
+	printf("Enter Marks of each subject \n");
+	scanf("%f%f%f%f%f",&s1,&s2,&s3,&s4,&s5);
+	sum = (s1+s2+s3+s4+s5);
+	float percentage = (sum/500)*100;
+	printf("Percentage of these five subjects is : %f",percentage);
+}
